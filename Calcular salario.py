6b5e7fd5d0b52e0867f490b38433a616c7aef220
kamby <marcosxmt@gmail.com>
2023-06-13 21:21:21 -0400

    "Up to date some files"

--- Calcular salario.py
+++ Calcular salario.py
@@ -1,4 +1,7 @@
-horTrab = int(input("Ingrese las horas trabajadas: "))
-varHoras = int(input("Ingrese el valor por hora: "))
+def calculateSalary():
+    horTrab = int(input("Ingrese las horas trabajadas: "))
+    varHoras = int(input("Ingrese el valor por hora: "))
 
-print(f"Sus horas trabajadas son {horTrab}, su valor por hora trabajadas es de {varHoras}, \nPor lo que su salario es de US$" + (str(horTrab * varHoras)) + " a la semana") 
+    print(f"Sus horas trabajadas son {horTrab}, su valor por hora trabajadas es de {varHoras}, \nPor lo que su salario es de US$" + (str(horTrab * varHoras)) + " a la semana") 
+
+calculateSalary()
